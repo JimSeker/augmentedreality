4b2e31d14a45cc56857de12be9afadbfe5baa233
JimSeker <seker@uwyo.edu>
2016-03-11 09:47:41 -0700

    updated to gradle1.5

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ augmented reality Examples for android.
 ================
 eclipse/ has the examples that eclipse can load (no longer updated).  Otherwise, in andriod studio.
 
-<b>basicARDemo</b> a "simple" example that turns on the camera and displays sensor info like gps over the image. 
+<b>basicARDemo</b> a "simple" example that turns on the camera and displays sensor info like gps over the image.  This app is need of a lot of updates, include adding permission checking, using camera2 APIs (or support camera if it ever comes out).  It was always intended to be a simple demo.
+     
 
 
 These are example code for University of Wyoming, Cosc 4730 Mobile Programming course. All examples are for Android.
--- basicARDemo/app/src/main/java/edu/cs4730/basicardemo/MainActivity.java
+++ basicARDemo/app/src/main/java/edu/cs4730/basicardemo/MainActivity.java
@@ -28,186 +28,187 @@ import android.widget.TextView;
  * https://github.com/JimSeker/video  piccapture demos.  for the camera preview.
  *
  * Yes, some many things are deprecated in 22... camera, orientation... I know.
+ * The camera permissions are also not included.
  * 
  */
 
 
-public class MainActivity extends Activity implements SensorEventListener, LocationListener, SurfaceHolder.Callback{
+public class MainActivity extends Activity implements SensorEventListener, LocationListener, SurfaceHolder.Callback {
 
-	
-	TextView  tv_alt, tv_lat, tv_long; //gps 
-	TextView  tv_head, tv_pitch, tv_roll; //orientation
-	TextView tv_x, tv_y, tv_z;  //ACCELEROMETER
-	
-	//sensor and gps
-	private LocationManager myL;
-	private SensorManager mgr;
-	private Sensor accel, orient;
-	
-	//camera and preview 
-	SurfaceView cameraPreview;
-	SurfaceHolder previewHolder;
-	Camera camera;
-	boolean inPreview;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		//display setup stuff
+
+    TextView tv_alt, tv_lat, tv_long; //gps
+    TextView tv_head, tv_pitch, tv_roll; //orientation
+    TextView tv_x, tv_y, tv_z;  //ACCELEROMETER
+
+    //sensor and gps
+    private LocationManager myL;
+    private SensorManager mgr;
+    private Sensor accel, orient;
+
+    //camera and preview
+    SurfaceView cameraPreview;
+    SurfaceHolder previewHolder;
+    Camera camera;
+    boolean inPreview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //display setup stuff
         tv_alt = (TextView) findViewById(R.id.altitudeValue);
         tv_lat = (TextView) findViewById(R.id.latitudeValue);
         tv_long = (TextView) findViewById(R.id.longitudeValue);
-        
+
         tv_head = (TextView) findViewById(R.id.headingValue);
         tv_pitch = (TextView) findViewById(R.id.pitchValue);
         tv_roll = (TextView) findViewById(R.id.rollValue);
-        
-		tv_x = (TextView) findViewById(R.id.xAxisValue);
+
+        tv_x = (TextView) findViewById(R.id.xAxisValue);
         tv_y = (TextView) findViewById(R.id.yAxisValue);
         tv_z = (TextView) findViewById(R.id.zAxisValue);
 
-		//we need the sensor manager and the gps manager, the 
-		//registration is all in onpause and onresume;
-		mgr = (SensorManager) this.getSystemService(SENSOR_SERVICE);
-		//orientation 
-		orient = mgr.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-		//acceleraometer
-		accel = mgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		//gps location information
-		myL = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		//setup the location listener.
+        //we need the sensor manager and the gps manager, the
+        //registration is all in onpause and onresume;
+        mgr = (SensorManager) this.getSystemService(SENSOR_SERVICE);
+        //orientation
+        orient = mgr.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+        //acceleraometer
+        accel = mgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //gps location information
+        myL = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //setup the location listener.
 
 
-		//all the camera preivew information
+        //all the camera preivew information
         inPreview = false;
-        cameraPreview = (SurfaceView)findViewById(R.id.cameraPreview);
+        cameraPreview = (SurfaceView) findViewById(R.id.cameraPreview);
         previewHolder = cameraPreview.getHolder();
         previewHolder.addCallback(this);
         previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-		
-	}
 
-	@Override
-	protected void onResume() {
-		mgr.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
-		mgr.registerListener(this, orient, SensorManager.SENSOR_DELAY_NORMAL);
-		myL.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-		super.onResume();
-	    camera=Camera.open();
-	}
-	@Override
-	protected void onPause() {
-		mgr.unregisterListener(this, accel);
-		mgr.unregisterListener(this, orient);
-		myL.removeUpdates(this);
-		super.onPause();
-	    camera.release();
-	    camera=null;
-	    inPreview=false;
-	}
-	
-	//this are for the Sensor events of orientation and accelerometer
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-
-		if (event.sensor.getType() == Sensor.TYPE_ORIENTATION)	{
-			tv_head.setText(String.valueOf(event.values[0]));
-			tv_pitch.setText(String.valueOf( event.values[1]));
-			tv_roll.setText(String.valueOf(event.values[2]));	
-		}  else if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-			tv_x.setText(String.valueOf(event.values[0]));
-			tv_y.setText(String.valueOf(event.values[1]));
-			tv_z.setText(String.valueOf(event.values[2]));
-		}
-	}
-
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		// ignoring!
-	}
-
-	//this are for the gps.
-	@Override
-	public void onLocationChanged(Location location) {
-		//if we have location information, update the screen here. just lat and lot, others
-		//are shown if you may need them.
-		if (location != null) {
-			tv_alt.setText("Altitude: " + (location.getAltitude() *3.2808399) + " ft");  //1 meter is 3.2808399 feet
-			tv_lat.setText("Latitude: " + location.getLatitude());
-			tv_long.setText("Longitude: " + location.getLongitude());
-		}	
-	}
-
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// ignoring!
-	}
-
-	@Override
-	public void onProviderEnabled(String provider) {
-		// ignoring!
-	}
-
-	@Override
-	public void onProviderDisabled(String provider) {
-		// ignoring!
-	}
-
-	
-	//finally the following are for the Camera preview.
-    private Camera.Size getBestPreviewSize(int width, int height, Camera.Parameters parameters) {
-    	Camera.Size result=null;
-
-    	for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
-    		if (size.width<=width && size.height<=height) {
-    			if (result==null) {
-    				result=size;
-    			}
-    			else {
-    				int resultArea=result.width*result.height;
-    				int newArea=size.width*size.height;
-
-    				if (newArea>resultArea) {
-    					result=size;
-    				}
-    			}
-    		}
-    	}
-
-    	return(result);
     }
-	@Override
-	public void surfaceCreated(SurfaceHolder holder) {
-		try {
-			camera.setPreviewDisplay(previewHolder);	
-		}
-		catch (Throwable t) {
-			Log.e("Camera", "Exception in setPreviewDisplay()", t);
-		}
-		
-	}
 
-	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width,	int height) {
-		Camera.Parameters parameters=camera.getParameters();
-		Camera.Size size=getBestPreviewSize(width, height, parameters);
+    @Override
+    protected void onResume() {
+        mgr.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+        mgr.registerListener(this, orient, SensorManager.SENSOR_DELAY_NORMAL);
+        myL.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        super.onResume();
+        camera = Camera.open();
+    }
 
-		if (size!=null) {
-			parameters.setPreviewSize(size.width, size.height);
-			camera.setParameters(parameters);
-			camera.startPreview();
-			inPreview=true;
-		}
-		
-	}
+    @Override
+    protected void onPause() {
+        mgr.unregisterListener(this, accel);
+        mgr.unregisterListener(this, orient);
+        myL.removeUpdates(this);
+        super.onPause();
+        camera.release();
+        camera = null;
+        inPreview = false;
+    }
 
-	@Override
-	public void surfaceDestroyed(SurfaceHolder holder) {
-		// TODO Auto-generated method stub
-		
-	}
+    //this are for the Sensor events of orientation and accelerometer
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_ORIENTATION) {
+            tv_head.setText(String.valueOf(event.values[0]));
+            tv_pitch.setText(String.valueOf(event.values[1]));
+            tv_roll.setText(String.valueOf(event.values[2]));
+        } else if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            tv_x.setText(String.valueOf(event.values[0]));
+            tv_y.setText(String.valueOf(event.values[1]));
+            tv_z.setText(String.valueOf(event.values[2]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // ignoring!
+    }
+
+    //this are for the gps.
+    @Override
+    public void onLocationChanged(Location location) {
+        //if we have location information, update the screen here. just lat and lot, others
+        //are shown if you may need them.
+        if (location != null) {
+            tv_alt.setText("Altitude: " + (location.getAltitude() * 3.2808399) + " ft");  //1 meter is 3.2808399 feet
+            tv_lat.setText("Latitude: " + location.getLatitude());
+            tv_long.setText("Longitude: " + location.getLongitude());
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // ignoring!
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // ignoring!
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // ignoring!
+    }
+
+
+    //finally the following are for the Camera preview.
+    private Camera.Size getBestPreviewSize(int width, int height, Camera.Parameters parameters) {
+        Camera.Size result = null;
+
+        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+            if (size.width <= width && size.height <= height) {
+                if (result == null) {
+                    result = size;
+                } else {
+                    int resultArea = result.width * result.height;
+                    int newArea = size.width * size.height;
+
+                    if (newArea > resultArea) {
+                        result = size;
+                    }
+                }
+            }
+        }
+
+        return (result);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            camera.setPreviewDisplay(previewHolder);
+        } catch (Throwable t) {
+            Log.e("Camera", "Exception in setPreviewDisplay()", t);
+        }
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        Camera.Parameters parameters = camera.getParameters();
+        Camera.Size size = getBestPreviewSize(width, height, parameters);
+
+        if (size != null) {
+            parameters.setPreviewSize(size.width, size.height);
+            camera.setParameters(parameters);
+            camera.startPreview();
+            inPreview = true;
+        }
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
